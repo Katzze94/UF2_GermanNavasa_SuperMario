7a9cc7e6d6b9836f81a41e679daced24120a02f9
Katzze94 <germannavasa2003@gmail.com>
2024-01-17 10:51:32 +0100

    Commit Jump Script

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,11 +4,23 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    public Rigidbody2D rBody;
+   
     public Vector3 newPosition = new Vector3(50, 5, 0);
 
     public float movementSpeed = 5;
+    public float jumpForce = 10;
+
     private float inputHorizontal;
+
     public bool jump = false;
+
+    public GroundSensor sensor;
+
+void Awake()
+{
+    rBody = GetComponent<Rigidbody2D>();
+}
 
     // Start is called before the first frame update
     void Start()
@@ -25,9 +37,11 @@ public class PlayerMovement : MonoBehaviour
     {
         inputHorizontal = Input.GetAxis("Horizontal");
         //transform.position = transform.position + new Vector3(1, 0, 0) * movementSpeed * Time.deltaTime;
-        transform.position += new Vector3(inputHorizontal, 0, 0) * movementSpeed * Time.deltaTime;
+        // transform.position += new Vector3(inputHorizontal, 0, 0) * movementSpeed * Time.deltaTime;
 
-        if(jump == true)
+        
+
+       /*if(jump == true)
         {
             Debug.Log("estoy saltando");
         }
@@ -39,6 +53,18 @@ public class PlayerMovement : MonoBehaviour
         else
         {
             Debug.Log("hi");
+        }*/
+        if(Input.GetButtonDown("Jump") && sensor.isGrounded == true)
+        {   
+            
+            rBody.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+
         }
+    }
+
+    void FixedUpdate()
+    {
+        rBody.velocity = new Vector2(inputHorizontal * movementSpeed, rBody.velocity.y);
+    
     }
 }
