dca5f53285ae4a5f5f1a6175d73ba7010b719c01
Katzze94 <germannavasa2003@gmail.com>
2024-04-03 10:25:58 +0200

    commit Arrays y spawnEnemy correción

---
+++ Assets/Scripts/EnemySpawn.cs
@@ -0,0 +1,67 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawn : MonoBehaviour
+{
+    public GameObject[] enemyPrefab;
+
+    public Transform[] spawnPoint;
+
+    public float spawnRate = 0.5f;
+
+    public float timer;
+
+    public float waitTime = 3f;
+
+    private bool activateSpawn = false;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(activateSpawn)
+        {
+            SpawnEnemy();
+        }
+    }
+
+    void SpawnEnemy()
+    {
+        timer += Time.deltaTime;
+
+        if(timer >= waitTime)
+        {
+            /*Instantiate(enemyPrefab[random.Range(0, enemyPrefab.Length)], spawnPoint[0].position, spawnPoint[0].rotation);
+            Instantiate(enemyPrefab[random.Range(0, enemyPrefab.Length)], spawnPoint[1].position, spawnPoint[1].rotation);
+            Instantiate(enemyPrefab[random.Range(0, enemyPrefab.Length)], spawnPoint[2].position, spawnPoint[2].rotation);*/
+
+            Instantiate(enemyPrefab[Random.Range(0, enemyPrefab.Length)], spawnPoint[Random.Range(0, spawnPoint.Length)].position, spawnPoint[Random.Range(0, spawnPoint.Length)].rotation);
+
+            timer = 0;
+        }
+    }
+    
+
+    void OnTriggerEnter2D(Collider2D collider)
+    {
+        if(collider.gameObject.tag == "Player")
+        {
+            activateSpawn = true;
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D collider)
+    {
+        if(collider.gameObject.tag == "Player")
+        {
+            activateSpawn = false;
+        }
+    }
+}
--- Assets/Scripts/ScriptDePrueba.cs
+++ Assets/Scripts/ScriptDePrueba.cs
@@ -13,31 +13,70 @@ public class ScriptDePrueba : MonoBehaviour
 
     private bool interruptor = false;
 
+    public int[] arrayNumeros;
+
+    private int[] arrayNumeros2 = new int[6];
+
+    private int[] arrayNumeros3 = {7, 8, 3, 9};
+
+    private string[] arrayStrings = new string[7];
+
+    private string[] arrayStrings2 = {"Hola", "Adios" };
+
+    private int[,] array2Dimensiones = new int[4, 2];
+    //lo mismo estas dos 
+    private int[,] array2Dimensiones2 = {{7, 8, 65, 0}, {9, 2, 545, 8}, {9, 6, 7, 7} };   
+
+    public List<string> stringList;
+
+    private List<int> intList = new List<int>(7);
+    private List<int> intList2 = new List<int>() {7, 9, 6, 78, 25, 02, 8, 78};
 
 
     // Start is called before the first frame update
     void Start()
     {
-        vidasPersonaje = 10;
-        numeroDecimal = 7.54f;
-        nombrePersonaje = "digrefg";
-        interruptor = true;
+        Debug.Log(arrayNumeros[1]);
+        Debug.Log(arrayNumeros[0]);
 
-        int suma = vidasPersonaje + vidasPersonaje2;
-        Debug.Log(suma);
-        string summaTextos = nombrePersonaje + "hula";
+        Debug.Log(array2Dimensiones2[0, 1]);
 
+        arrayNumeros2[0] = 4;
+        arrayNumeros2[5] = 3;
 
+        array2Dimensiones2[2, 1] = 777777;
 
-        Debug.Log(summaTextos);
-        Debug.Log(nombrePersonaje);
-        Debug.Log(vidasPersonaje);
-        Debug.Log(numeroDecimal);
+        intList2.Add(10);
+
+        intList2.Insert(5, 888);
+        intList2.RemoveAt(2);
+        intList2.Remove(78);
+
+        intList2.Clear();
     }
+
+        //vidasPersonaje = 10;
+       // numeroDecimal = 7.54f;
+       // nombrePersonaje = "digrefg";
+       // interruptor = true;
+
+       // int suma = vidasPersonaje + vidasPersonaje2;
+       // Debug.Log(suma);
+       // string summaTextos = nombrePersonaje + "hula";
+
+
+
+      //  Debug.Log(summaTextos);
+       // Debug.Log(nombrePersonaje);
+       // Debug.Log(vidasPersonaje);
+      //  Debug.Log(numeroDecimal);
+
+    
 
     // Update is called once per frame
     void Update()
     {
   
-    }
+   }
 }
+
